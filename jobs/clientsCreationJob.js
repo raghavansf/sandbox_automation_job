9dab624ed3bb4c0283cb2f0d5f0c32db8827aa99
raghavansf <100221094+raghavansf@users.noreply.github.com>
2022-09-15 12:21:40 +0100

    Updated Axios response Destructuring

--- jobs/clientsCreationJob.js
+++ jobs/clientsCreationJob.js
@@ -1,13 +1,23 @@
-import SandboxMgr from '../sandboxMgr.js';
+import ClientMgr from '../clientMgr.js';
+import {} from 'dotenv/config';
+
+// its TEST!!!
 async function createAPIClients() {
   try {
-    const sandboxMgr = new SandboxMgr();
-    const provisionRequest = {};
-    const sandboxDetails = await sandboxMgr.provisionNewSandbox(
-      provisionRequest
+    const ClientCredentials = {
+      clientID: process.env.ADMIN_CLIENT_ID,
+      clientSecret: process.env.ADMIN_CLIENT_PASSWORD,
+      grantType: { grant_type: 'client_credentials' },
+    };
+    const clientMgr = new ClientMgr();
+    const accessToken = await clientMgr.getAccessToken();
+    console.log('Access Token ', accessToken);
+    const credentialAccessToken = await clientMgr.getAccessTokenByCredentials(
+      ClientCredentials
     );
+    console.log('Client Credentials Access Token ', credentialAccessToken);
   } catch (Error) {
-    console.log('Error occured as part of Client creation', Error.stack);
+    console.log('Error occured as part of Client creation', error);
   }
 }
 
