c601af593d74f10024254427cdf6cc939447f066
Alina Condurachi <acondurachi@salesforce.com>
2022-10-05 12:43:39 +0100

    s3.getObject_passedLocal

--- jobs/importJob.js
+++ jobs/importJob.js
@@ -3,20 +3,19 @@ import {} from 'dotenv/config';
 
 
 var s3  = new AWS.S3({
-    accessKeyId: process.env.BUCKETEER_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.BUCKETEER_AWS_SECRET_ACCESS_KEY,
-    region: process.env.BUCKETEER_AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.STORAGE_REGION,
+    s3ForcePathStyle: true
 });
 
 var params = {
-    Key: '/public/SFRA_CODE/SFRA_Sandbox.zip',
-    Bucket: process.env.BUCKETEER_BUCKET_NAME
+    Key: 'public/SFRA_CODE/SFRA_Sandbox.zip',
+    Bucket: process.env.NODE_ENV === 'development' ? process.env.STORAGE_BUCKET_NAME : process.env.BUCKETEER_BUCKET_NAME
 };
 
 s3.getObject(params, function(err, data) {
     if (err) console.log(err, err.stack);
     else console.log('The ObjectOutput from S3 - ', data);
 });
-
-
 
