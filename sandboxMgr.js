a72c6bf9c0028f681918e7e87c7fcc1b73674904
raghavansf <100221094+raghavansf@users.noreply.github.com>
2022-11-03 20:26:55 +0000

    Changed the Polling interval

--- sandboxMgr.js
+++ sandboxMgr.js
@@ -114,7 +114,7 @@ export default class SandboxMgr {
                 'Job execution Not Completed hence waiting ......',
                 response.status
               );
-            }, 10000);
+            }, 60000);
           }
         }
       }
